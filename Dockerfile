1fa028306c22e94ca3f48637b0de101aa148aa78
Anushka Swarup <anushkaswarup@Anushkas-MacBook-Pro.local>
2020-03-13 14:30:25 -0400

    serverside cleaning

--- Dockerfile
+++ Dockerfile
@@ -1,43 +1,41 @@
-# this is our first build stage, it will not persist in the final image
-FROM ubuntu as intermediate
+FROM alpine as builder
+RUN apk add --no-cache openssh-client git
 
-# install git
-RUN apt-get update
-RUN apt-get install -y git
+COPY github_key .
 
-# add credentials on build
-ARG SSH_PRIVATE_KEY
-RUN mkdir /root/.ssh/
-RUN echo "${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa
+RUN eval $(ssh-agent) && \
+    ssh-add github_key && \
+    ssh-keyscan -H github.com >> /etc/ssh/ssh_known_hosts && \
+    git clone git@github.com:USDA-ARS-GBRU/predictPAM.git /opt/predictPAM
 
-# make sure your domain is accepted
-RUN touch /root/.ssh/known_hosts
-RUN ssh-keyscan bitbucket.org >> /root/.ssh/known_hosts
+WORKDIR /opt/predictPAM
 
-RUN git clone git@github.com:USDA-ARS-GBRU/predictPAM.git
 
 
 FROM ubuntu:16.04
 
 MAINTAINER Anushka Swarup "aswarup@ufl.edu"
 
-RUN apt-get update -y && \
-    apt-get install -y python-pip python-dev && \
-    pip install --upgrade pip
+RUN apt-get update && apt-get install -y \
+	python-pip python-dev \
+	build-essential libssl-dev libffi-dev\
+    libxml2-dev libxslt1-dev zlib1g-dev \
+    && pip install --upgrade pip
 
 COPY . /app
 
 WORKDIR ./app
 
+RUN pip install pip==9.0.3 pybind11
+
 RUN pip install -r ./app/requirements.txt
+
+COPY --from=builder /opt/predictPAM/ .
+EXPOSE 80
 
 ENTRYPOINT [ "python" ]
 
-CMD [ "app.py" ]
-
-# copy the repository form the previous image
-COPY --from=intermediate /your-repo /srv/your-repo
-# ... actually use the repo :)
+CMD [ "./app/app.py" ]
 
 
 
--- app/app.py
+++ app/app.py
@@ -5,15 +5,14 @@ from werkzeug import secure_filename
 import sys
 import pandas as pd
 import numpy as np
-from app.mod_tables.serverside.serverside_table import ServerSideTable
-from app.mod_tables.serverside import table_schemas
+from serverside.serverside_table import ServerSideTable
+from serverside import table_schemas
 import predictPAM.main
 from flask import Blueprint, jsonify, request
-from app import table_builder
 # import uuid 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLD = '/Users/anushkaswarup/Downloads/Storage/EPI/WebAppPAM/'
+UPLOAD_FOLD = '/Users/anushkaswarup/Downloads/Storage/EPI/WebAppPAM/app/'
 UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_FOLD)
 ALLOWED_EXTENSIONS = {'gbk','gb'}
 
@@ -59,10 +58,7 @@ def handle_form():
         gbkfile = UPLOAD_FOLD+file.filename
         gbkfileloc.append(gbkfile)
 
-    # gbkfileloc = ["/Users⁩/aswarup⁩/Downloads⁩/Storage/⁨Github_repos⁩/predictPAM-WebApp⁩/Burkholderia_thailandensis_E264__ATCC_700388_133.gbk"]
-    print(gbkfileloc)
-    
-    out = request.form['Output'] 
+    out = request.form['Output']
 
     data = {"gbkfile":gbkfileloc, "pamseq":pamSeq, "targetlength":tarLength, 
     "strand":strand_str, "lcp":lcp,"eds":eds, "outfile":out, "tempdir":None, "threads":1, "log":"predictpam.log"}
@@ -84,48 +80,6 @@ def serverside_table_content():
     df = pd.read_csv(out, sep="\t", header=0)
 
     data_dict = df.fillna(0).to_dict('records')
-    # a = [{
-    #   "accession_x": "NC_007650", 
-    #   "accession_y": "NC_007650", 
-    #   "db_xref_x": "['GeneID:3844926']", 
-    #   "db_xref_y": "['GeneID:3844868']", 
-    #   "distaltopam": "GGCAAGACTTGT", 
-    #   "distance_x": 1189, 
-    #   "distance_y": 0, 
-    #   "gene_synonym_x": 0, 
-    #   "gene_synonym_y": 0, 
-    #   "locus_tag_x": "['BTH_II0002']", 
-    #   "locus_tag_y": "['BTH_II0001']", 
-    #   "name_x": 0, 
-    #   "name_y": 0, 
-    #   "note_x": 0, 
-    #   "note_y": 0, 
-    #   "pam_seq": "ATGC", 
-    #   "product_name_confidence_x": 0, 
-    #   "product_name_confidence_y": 0, 
-    #   "product_synonym_x": 0, 
-    #   "product_synonym_y": 0, 
-    #   "product_x": 0, 
-    #   "product_y": 0, 
-    #   "protein_id_x": 0, 
-    #   "protein_id_y": 0, 
-    #   "proxitopam": "CGCTGCACGC", 
-    #   "seqid": "NC_007650", 
-    #   "start_x": 1280, 
-    #   "start_y": 0, 
-    #   "stop_x": 2324, 
-    #   "stop_y": 1188, 
-    #   "strand": "forward", 
-    #   "strand_for_feature_x": "reverse", 
-    #   "strand_for_feature_y": "forward", 
-    #   "target": "GGCAAGACTTGTCGCTGCACGC", 
-    #   "target_ep": 92, 
-    #   "target_sp": 70, 
-    #   "translation_x": 0, 
-    #   "translation_y": 0, 
-    #   "type_x": "gene", 
-    #   "type_y": "gene"
-    # }]
 
     columns = table_schemas.SERVERSIDE_TABLE_COLUMNS
     data = ServerSideTable(request, data_dict, columns).output_result()
